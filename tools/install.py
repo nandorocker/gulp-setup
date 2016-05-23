3b235d683a0bb182eb7b9a03bc275521f5aab5d5
Nando Rossi <talk@nan.do>
2016-05-23 18:15:08 +0200

    adds setup script for scaffolding etc

---
+++ tools/install.py
@@ -0,0 +1,22 @@
+import os
+import errno
+
+#
+# Create base directores
+#
+
+# Directory array
+pathList = ['app','app/assets','app/pages','app/scripts','app/styles']
+
+# Path creation function
+def mkdir_p(path):
+    try:
+        os.makedirs(path)
+    except OSError as exc:  # Python >2.5
+        if exc.errno == errno.EEXIST and os.path.isdir(path):
+            pass
+        else:
+            raise
+
+for path in pathList:
+    mkdir_p(path)
